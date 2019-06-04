c008b48f9f0779add7bbad1c74a7971d47d67bff
Wouter Govaert <wouter@stylelabs.com>
2019-06-04 09:39:01 +0200

    start

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            quiz_position: 1
-        }
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      quiz_position: 1
+    };
+  }
   render() {
-    return <div><div class="quizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>;
+    return (
+      <div class="quizQuestion">
+        {quizData.quiz_questions[0].instruction_text}
+      </div>
+    );
   }
 }
 
