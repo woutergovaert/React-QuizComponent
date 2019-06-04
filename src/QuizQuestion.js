d4fdb0366df77b4a09fbc2b8396250b988bbc679
Wouter Govaert <wouter@stylelabs.com>
2019-06-04 09:56:16 +0200

    start

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class QuizQuestion extends Component {
-    render(){
-        return(
-            <main>
-            <section>
-              <p>// instruction text goes here</p>
-            </section>
-            <section className="buttons">
-              <ul>
-                //quiz question button logic goes here
-              </ul>
-            </section>
-          </main>
-        )
-    }
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default QuizQuestion;
