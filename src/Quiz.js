a5a05da28e441c91c7930ced4b03a47dc29f958c
Wouter Govaert <wouter@stylelabs.com>
2019-06-04 13:31:53 +0200

    changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,7 +16,7 @@ class Quiz extends Component {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        { isQuizEnd && <QuizEnd />}
+        { isQuizEnd == true && <QuizEnd />}
         <QuizQuestion
           quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
         />
