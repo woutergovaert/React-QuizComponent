f8359f0ac829f6f762e7d24c09f550771d84d798
Wouter Govaert <wouter@stylelabs.com>
2019-06-04 13:33:17 +0200

    changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,17 +9,21 @@ class Quiz extends Component {
     this.state = {
       quiz_position: 1
     };
-  };
-
+  }
 
   render() {
-    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        { isQuizEnd == true && <QuizEnd />}
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
+        {isQuizEnd && <QuizEnd />}
+        {!isQuizEnd && (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
